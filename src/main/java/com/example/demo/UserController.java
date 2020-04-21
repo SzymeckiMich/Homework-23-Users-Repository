b45dd5e5142c32c1c7c42e0afd32adbe95fd2aef
Anna Biga <aniabiga040896@gmail.com>
2020-04-21 15:37:08 +0200

    create project. Code works but only on Get method

---
+++ src/main/java/com/example/demo/UserController.java
@@ -0,0 +1,54 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+@Controller
+public class UserController {
+
+    private UsersRepository usersRepository;
+
+    //    @Autowired
+    public UserController(UsersRepository usersRepository) {
+        this.usersRepository = usersRepository;
+    }
+
+
+    //    @GetMapping("/users")
+//    @PostMapping("/users")
+    @ResponseBody
+    @RequestMapping(value = "/users", method = RequestMethod.GET)
+    public String users() {
+        List<User> users = usersRepository.getAll();
+
+        String result = "";
+
+
+        for (int i = 0; i < users.size(); i++) {
+            result += users.get(i).toString() + "</br>";
+        }
+        return result;
+    }
+
+    @RequestMapping("/add")
+    public String addUser(HttpServletRequest request) {
+        String firstName = request.getParameter("firstName");
+        String lastName = request.getParameter("lastName");
+        String age = request.getParameter("age");
+
+        int ageInt = Integer.parseInt(age);
+
+        if (StringUtils.isEmpty(firstName)) {
+            return "/error.html";
+        } else {
+            User user = new User(firstName, lastName, ageInt);
+            usersRepository.add(user);
+            return "/success.html";
+        }
+    }
+}
---
+++ src/main/java/com/example/demo/UsersRepository.java
@@ -0,0 +1,29 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class UsersRepository {
+
+
+    private List<User> users;
+
+    @Autowired
+    public UsersRepository() {
+        users = new ArrayList<>();
+        users.add(new User("Michał", "Kowalski", 23));
+    }
+
+    public List<User> getAll() {
+        return new ArrayList<>(users);
+    }
+
+    public void add(User user) {
+        users.add(user);
+    }
+
+}
